4a4fe9419284396cc1d37ffe1960a445331aab92
Vinayak Pillai <vinayak_p@Vinayaks-MacBook-Air.local>
2026-03-03 23:21:30 +0530

    update: added the utils fn of fetch emi

--- utils/fetch-fns.ts
+++ utils/fetch-fns.ts
@@ -1,5 +1,5 @@
 import { db } from "@/db";
-import { accounts } from "@/db/schema";
+import { accounts, emi } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
 export const fetchActiveAccounts = async () => {
@@ -13,6 +13,24 @@ export const fetchActiveAccounts = async () => {
       })
       .from(accounts)
       .where(eq(accounts.isActive, true));
+    return records;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+export const fetchEmis = async () => {
+  try {
+    const records = await db
+      .select({
+        id: emi.id,
+        name: emi.name,
+        amount: emi.amount,
+        accountId: emi.accountId,
+      })
+      .from(emi)
+      .where(eq(emi.isActive, true));
     return records;
   } catch (error) {
     console.error(error);
